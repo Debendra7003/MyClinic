0200d893957d189546575a73a6d5a3989477ac07
DEBENDRA <161910591+Debendra7003@users.noreply.github.com>
2025-04-25 12:42:45 +0530

    addd commentline

--- MyClinic/AmbulanceAccess/views.py
+++ MyClinic/AmbulanceAccess/views.py
@@ -12,7 +12,7 @@ from LoginAccess.models import User
 
 # Create your views here.
 
-
+# ----------------------------------- New Ambulance Register ------------------------------------------------
 class AmbulanceView(APIView):
     permission_classes=[AllowAny]
     def post(self, request):
@@ -23,7 +23,7 @@ class AmbulanceView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
+# ------------------------------------ Update ambulance active to inactiv or inactive to active-----------------
 class ToggleAmbulanceStatusView(APIView):
     permission_classes=[AllowAny]
     def put(self, request, ambulance_id, vehicle_number):
@@ -32,14 +32,12 @@ class ToggleAmbulanceStatusView(APIView):
             ambulance = Ambulance.objects.get(ambulance_id=user, vehicle_number=vehicle_number)
         except Ambulance.DoesNotExist:
             return Response({"error": "Ambulance not found with provided ambulance_id and vehicle_number."}, status=status.HTTP_404_NOT_FOUND)
-
         ambulance.active = not ambulance.active  # Toggle active status
         ambulance.save()
-
         return Response({
             "message": f"Ambulance status updated to {'Active' if ambulance.active else 'Inactive'}",}, status=status.HTTP_200_OK)
     
-
+# ------------------------------------ Get Total count of active or Inctive ambulance----------------------------
 class AmbulanceByUserView(APIView):
     permission_classes=[AllowAny]
     def get(self, request, ambulance_id):
@@ -56,7 +54,7 @@ class AmbulanceByUserView(APIView):
             "inactive_count": inactive_ambulances.count()
         }, status=status.HTTP_200_OK)
     
-
+# ------------------------------------Get Active or Iactive Ambulance----------------------------
 class AmbulanceStatusFilterView(APIView):
     permission_classes=[AllowAny]
     def get(self, request):
@@ -80,7 +78,7 @@ class AmbulanceStatusFilterView(APIView):
             "ambulances": serializer.data
         }, status=status.HTTP_200_OK)
 
-
+# ------------------------------------Ambulance Delete --------------------------------------------
 class AmbulanceDeleteView(APIView):
     permission_classes=[AllowAny]
     def delete(self, request, ambulance_id, vehicle_number):
@@ -95,6 +93,8 @@ class AmbulanceDeleteView(APIView):
         return Response({
             "message": f"Ambulance with vehicle '{vehicle_number}' deleted successfully"}, status=status.HTTP_200_OK)
     
+
+# -------------------------------------Areawise ambulance search-------------------------------------
 class AmbulanceSearchByAreaView(APIView):
     permission_classes=[AllowAny]
     def get(self, request, service_area):
