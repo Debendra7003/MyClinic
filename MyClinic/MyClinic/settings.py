e898d63f0bbee6b207b98fde1d2f4a1e6f9ea4b4
sidharth-droid <sidharth2432004@gmail.com>
2025-06-18 16:00:48 +0530

    dbsqlite3

--- MyClinic/MyClinic/settings.py
+++ MyClinic/MyClinic/settings.py
@@ -99,18 +99,18 @@ WSGI_APPLICATION = 'MyClinic.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQL_DATABASE'),
-        'USER': os.getenv('MYSQL_USER'),
-        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': os.getenv('MYSQL_HOST','db'),
-        'PORT': os.getenv('MYSQL_PORT', '3306')
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': os.getenv('MYSQL_DATABASE'),
+    #     'USER': os.getenv('MYSQL_USER'),
+    #     'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+    #     'HOST': os.getenv('MYSQL_HOST','db'),
+    #     'PORT': os.getenv('MYSQL_PORT', '3306')
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
